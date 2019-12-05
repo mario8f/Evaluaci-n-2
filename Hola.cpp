231c295ed8668deb1ed88cd5bf1d2ba8fe4e427a
mario8f <55576317+mario8f@users.noreply.github.com>
2019-12-05 09:14:09 +0100

    Add files via upload

---
+++ Hola.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+int main(){
+    //Definición de variables
+    char salir;
+    char respuesta;
+    std::cout<<"Hola mundo, mi primer programa \n";
+    std::cout <<"Eres guapo? (s/n)";
+    std::cin >>respuesta;
+    //&& significa AND
+    //|| significa OR
+    if(respuesta=='s'){
+        std::cout <<"Hola guapo";
+    }
+    else{
+         std::cout <<"Hola feo";
+    }
+    std::cin >> salir;
+    
+    
+}
+    
+    
