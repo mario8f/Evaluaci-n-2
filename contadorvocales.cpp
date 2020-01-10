d231d130b13477a27af05ff6708cf872461d2be4
mario8f <55576317+mario8f@users.noreply.github.com>
2020-01-10 09:18:50 +0100

    Add files via upload

---
+++ contadorvocales.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+/*Un programa que lea una palabra y sustituya las vocales por 'u'*/
+int devuelve_longitud(char palabra[]){
+    int longitud=0;//variable acumuladora
+    int cont=0;
+    while(palabra[cont]!='\0'){
+          longitud++;
+          cont++;
+    }
+    return(longitud);
+}
+
+int main(){
+    char palabra[10];
+    char vocales[]={'a','e','i','o','u'};
+    char salir;                                                                                                                                                                                                                                 
+    int cont,nvocales;
+    int cont2=0;
+    int longitud=0;
+    
+    std::cout<<"Escriba una palabra ";
+    std::cin>>palabra;
+    for(cont=0;cont<devuelve_longitud(palabra);cont++){
+     for(nvocales=0;nvocales<5;nvocales++){
+      if(palabra[cont]==vocales[nvocales]){
+           cont2++;
+           
+      }
+     }
+    }
+    std::cout<<cont2;
+    std::cin>>salir;
+    
+    
+}
+    
