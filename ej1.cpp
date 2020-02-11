10832a1d2f9b00e229c775f2172c8a0614506c04
mario8f <55576317+mario8f@users.noreply.github.com>
2020-02-11 11:20:24 +0100

    Add files via upload

---
+++ ej1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+char salir;
+int solucion;
+int a,b,c,d;
+std::cout<<"Dime cuatro numeros \n";
+std::cin>>a;
+std::cin>>b;
+std::cin>>c;
+std::cin>>d;
+solucion=a*b*c*d;
+std::cout<<solucion;
+std::cin>>salir;    
+}
